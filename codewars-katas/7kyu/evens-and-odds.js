93581b9eaad3a702d4f572e4c3c6007b0eb4e5cd
chinwuba Israel <chiizzy5@gmail.com>
2022-06-16 09:05:43 +0100

    7kyu evens and odds challenge complete

---
+++ codewars-katas/7kyu/evens-and-odds.js
@@ -0,0 +1,19 @@
+
+  /*  *****
+Challenge: Evens and Odds
+
+Instructions: This kata is about converting numbers to their binary or hexadecimal representation:
+
+If a number is even, convert it to binary.
+If a number is odd, convert it to hex.
+Numbers will be positive. The hexadecimal string should be lowercased.
+              
+
+                                       MY Solution
+function evensAndOdds(num){
+    return num % 2 === 0 ? (num).toString(2) : (num).toString(16)
+  }
+
+
+                                       CodeWars Solution(s)
+*/
