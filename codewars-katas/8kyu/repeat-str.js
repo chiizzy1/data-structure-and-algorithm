7f411f162b07b7b1e08c75935ca6c137fbf80e08
chinwuba Israel <chiizzy5@gmail.com>
2022-06-17 07:47:40 +0100

    8k string challenge complete

---
+++ codewars-katas/8kyu/repeat-str.js
@@ -0,0 +1,26 @@
+/*  *****
+Challenge: 8kyu String repeat
+
+Instructions: Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+
+Examples (input -> output)
+6, "I"     -> "IIIIII"
+5, "Hello" -> "HelloHelloHelloHelloHello"
+              
+
+                                       MY Solution
+
+                function repeatStr (n, s) {
+                let ans = ''
+                for (let i = 0; i < n; i++){
+                    ans += s;   
+                }
+                return ans
+                }
+               
+                                       CodeWars Solution(s)
+
+                function repeatStr (n, s) {
+                return s.repeat(n);
+                }
+*/
