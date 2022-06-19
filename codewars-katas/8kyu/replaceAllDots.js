b8e1492792a4c80f885634f71d25caa6a80b7b50
chinwuba Israel <chiizzy5@gmail.com>
2022-06-19 17:27:35 +0100

    8kyu challenge complete

---
+++ codewars-katas/8kyu/replaceAllDots.js
@@ -0,0 +1,15 @@
+/*  *****
+Challenge: : Replace all dots
+
+Instructions: The code provided is supposed replace all the dots . in the specified String str with dashes -
+But it's not working properly.
+Task
+Fix the bug so we can all go home early.    
+
+                                       MY Solution
+                var replaceDots = function(str) {
+                return str.replace(/\./g,'-');
+                }
+                                       CodeWars Solution(s)
+                var replaceDots = s=>s.split('.').join('-')
+*/
