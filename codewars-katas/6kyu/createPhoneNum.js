3abf1bd2604e3cf22368137af3022776624ee807
chinwuba Israel <chiizzy5@gmail.com>
2022-06-28 08:41:35 +0100

    6kyu challenge solved first tryyy

---
+++ codewars-katas/6kyu/createPhoneNum.js
@@ -0,0 +1,37 @@
+/*  *****
+Challenge: 6kyu 
+
+Instructions: 
+
+                                            MY Solution
+                          
+                                    function createPhoneNumber(numbers){
+                                        let a = numbers.filter((num, index) => index < 3 ).join('')
+                                        let b = numbers.filter((num, index) => index > 2 && index < 6 ).join('')
+                                        let c = numbers.filter((num, index) => index > 5 ).join('')
+                                        return `(${a}) ${b}-${c}`
+                                    }
+
+                                       CodeWars Solution(s)
+
+                           function createPhoneNumber(numbers){
+                            var phoneNumber = "(xxx) xxx-xxxx";
+                            for(var i = 0; i < numbers.length; i++) {
+                                //regex will only replace first x found
+                                phoneNumber = phoneNumber.replace('x', numbers[i]);
+                            }
+                            return phoneNumber;
+                            }
+
+                                       CodeWars Solution(2)
+
+                    function createPhoneNumber(numbers){
+                        return numbers.join('').replace(/(...)(...)(.*)/, '($1) $2-$3');
+                        }
+                                       CodeWars Solution(3)
+
+                         function createPhoneNumber(numbers){
+                            return numbers.join('').replace(/(\d{3})(\d{3})(\d{4})/,'($1) $2-$3');
+                        }
+
+*/
