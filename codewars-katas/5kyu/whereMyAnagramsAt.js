e5e3260a64622afe2a9850454be58e60f8d652f9
chinwuba Israel <chiizzy5@gmail.com>
2022-07-02 21:33:45 +0100

    5kyu challenge solved

---
+++ codewars-katas/5kyu/whereMyAnagramsAt.js
@@ -0,0 +1,43 @@
+/*  *****
+Challenge: 5 kyu
+Where my anagrams at?
+
+Instructions: What is an anagram? Well, two words are anagrams of each other if they both contain the same letters. For example:
+
+'abba' & 'baab' == true
+
+'abba' & 'bbaa' == true
+
+'abba' & 'abbba' == false
+
+'abba' & 'abca' == false
+Write a function that will find all the anagrams of a word from a list. You will be given two inputs a word and an array with words. You should return an array of all the anagrams or an empty array if there are none. For example:
+
+anagrams('abba', ['aabb', 'abcd', 'bbaa', 'dada']) => ['aabb', 'bbaa']
+
+anagrams('racer', ['crazer', 'carer', 'racar', 'caers', 'racer']) => ['carer', 'racer']
+
+anagrams('laser', ['lazing', 'lazy',  'lacer']) => []
+
+                                       MY Solution
+
+                    function anagrams(word, words) {
+                         return words.filter(each => each.split('').sort().join('') === word.split('').sort().join(''))
+                    }
+                    
+
+                                       CodeWars Solution(s)
+
+                String.prototype.sort = function() {
+                    return this.split("").sort().join("");
+                    };
+
+                    function anagrams(word, words) {
+                    return words.filter(function(x) {
+                        return x.sort() === word.sort();
+                    });
+                    }
+               
+
+*/
+
