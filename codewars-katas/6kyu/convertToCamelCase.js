d9edf8b621c8bba6ac66d4be1534196b9711431a
Chinwuba Israel <chiizzy5@gmail.com>
2022-07-06 10:14:31 +0100

    correct 6k solution updated

--- codewars-katas/6kyu/convertToCamelCase.js
+++ codewars-katas/6kyu/convertToCamelCase.js
@@ -10,18 +10,33 @@ Examples
 
                                        MY Solution
 
-                function toCamelCase(str){
-                    let arr = str.replaceAll('-', ' ').replaceAll('_', ' ').split(' ')
-                    return arr.map((word, index) => index === 0 ? word : word[0].toUpperCase() + word.slice(1)).join('')
+               function toCamelCase(str){
+                    str = str.split('-').join(' ')
+                    let arr = str.split('_').join(' ')
+                    return arr.split(' ').map((word, index) => index === 0 ? word : word[0].toUpperCase() + word.slice(1)).join('')
                 }
 
                                        CodeWars Solution(s)
                          function toCamelCase(str){
-      var regExp=/[-_]\w/ig;
-      return str.replace(regExp,function(match){
-            return match.charAt(1).toUpperCase();
-       });
-}
+                            var regExp=/[-_]\w/ig;
+                            return str.replace(regExp,function(match){
+                                    return match.charAt(1).toUpperCase();
+                            });
+                        }
+
+                        CodeWars Solution(2)
+            function toCamelCase(str){
+                var demarcs ='-_';
+                return str.split('').map(function(value,index,array){
+                    if(demarcs.indexOf(value) > -1){
+                    return '';
+                    } else if (demarcs.indexOf(array[index - 1]) > -1){
+                    return value.toUpperCase();
+                    } else {
+                    return value;
+                    }
+                }).join('');
+                }
                                
 */
 
