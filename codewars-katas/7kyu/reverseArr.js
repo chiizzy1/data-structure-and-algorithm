0b72caafb750370d776d875ff0d84ba839aaf865
Chinwuba Israel <chiizzy5@gmail.com>
2022-07-29 10:15:26 +0100

    site updated

---
+++ codewars-katas/7kyu/reverseArr.js
@@ -0,0 +1,12 @@
+/*  *****
+Challenge: 7 kyu
+Reverse list
+
+
+Instructions:  Write reverseList function that simply reverses lists.
+
+                                    MySolution
+               function reverseList(arr) {
+                   return arr.reverse()
+                }
+*/
