4e3a306430a6f496b738587d5e21ed9fe36933f8
Chinwuba Israel <chiizzy5@gmail.com>
2022-08-09 10:34:35 +0100

    8kyu challenge complete

--- codewars-katas/6kyu/areTheySame.js
+++ codewars-katas/6kyu/areTheySame.js
@@ -1,48 +1,49 @@
 /*  *****
-Challenge: 5kyu Rot13
+Challenge: 6 kyu
+Are they the "same"?
 
 
-Instructions:  
+Instructions:  Given two arrays a and b write a function comp(a, b) (orcompSame(a, b)) that checks whether the two arrays have the "same" elements, with the same multiplicities (the multiplicity of a member is the number of times it appears). "Same" means, here, that the elements in b are the elements in a squared, regardless of the order.
+
+Examples
+Valid arrays
+a = [121, 144, 19, 161, 19, 144, 19, 11]  
+b = [121, 14641, 20736, 361, 25921, 361, 20736, 361]
+comp(a, b) returns true because in b 121 is the square of 11, 14641 is the square of 121, 20736 the square of 144, 361 the square of 19, 25921 the square of 161, and so on. It gets obvious if we write b's elements in terms of squares:
+
+a = [121, 144, 19, 161, 19, 144, 19, 11] 
+b = [11*11, 121*121, 144*144, 19*19, 161*161, 19*19, 144*144, 19*19]
 
                                     MySolution
                      
-                            
-                    function rot13(str){
-                            let message = str.toLowerCase()
-                            const alphabet = "abcdefghijklmnopqrstuvwxyz".split('')
-                            let ans = ""
-                            for (let i = 0; i < message.length; i++){
-                                let char = message[i]
-                                if ( alphabet.includes(char) ){
-                                let index = alphabet.indexOf(char)
-                                let newIndex = ( index + 13 ) % 26
-                                char === str[i] ? ans += alphabet[newIndex] : ans += alphabet[newIndex].toUpperCase()
-                                }
-                                else{ ans += char }
-                            }
-                            
-                            return ans
+                function comp(a, b){
+                    if ( a === [] || b === [] || a === null|| b === null || a.length !== b.length ){ return false }
+                    a = a.sort( (a, b) => a- b)
+                    b = b.sort( (a, b) => a- b)
+                    for (let i = 0; i < b.length; i++){
+                        let num = b[i]
+                        if ( a[i] !== Math.sqrt(num) ){
+                            return false
+                        }
                     }
+                    return true
+                }                  
 
                                     CodeWar Solution(1)
 
-                    function rot13(message){
-                        //your code here
-                        return (message + '')
-                            .replace(/[a-z]/gi, function(s) {
-                            return String.fromCharCode(s.charCodeAt(0) + (s.toLowerCase() < 'n' ? 13 : -13));
-                            });
+        function comp(a, b) {
+            if (!a || !b || a.length !== b.length) return false;
+            return a.map(x => x * x).sort().toString() === b.sort().toString();
+        }
 
-                    }
                                     CodeWar Solution(2)
                 
-                function rot13(message){
-                    const abc = 'abcdefghijklmnopqrstuvwxyz';
-                    
-                    return [...message].map((i, idx) => {
-                        const index = abc.indexOf(i.toLowerCase()) ; 
-                        const l = index+13  < 26 ? abc[index+13] : abc[Math.abs(index +13 -26)] ; 
-                        return index === -1 ? i : i === i.toUpperCase() ? l.toUpperCase() : l; 
-                    }).join('');
+                function comp(array1, array2){
+                    try{
+                        var sortArray2=array2.map(Math.sqrt).sort();
+                        return JSON.stringify(array1.sort())===JSON.stringify(sortArray2);
+                    }catch(e){
+                        return false;
+                    }
                 }
 */
---
+++ codewars-katas/8kyu/nthEven.js
@@ -0,0 +1,23 @@
+/*  *****
+Challenge: 8 kyu
+Get Nth Even Number
+
+
+Instructions:  Example(Input --> Output)
+
+1 --> 0 (the first even number is 0)
+3 --> 4 (the 3rd even number is 4 (0, 2, 4))
+100 --> 198
+1298734 --> 2597466
+
+
+                                    MySolution
+                     
+                     function nthEven(n){
+                        return (n-1)*2
+                    }
+
+                                    CodeWar Solution(1)
+                    const nthEven=n=>n*2-2;
+        
+*/
