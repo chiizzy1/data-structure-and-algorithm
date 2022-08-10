42c5adafdb51a7fde2b48076d031120522942576
Chinwuba Israel <chiizzy5@gmail.com>
2022-08-10 19:49:53 +0100

    8kyu challenge complete

---
+++ codewars-katas/8kyu/booleanToString.js
@@ -0,0 +1,13 @@
+/*  *****
+Challenge: 8kyu  Convert boolean values to strings 'Yes' or 'No'.
+
+
+Instructions:  Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+                                    MySolution
+                     
+                   function boolToWord( bool ){
+                        return bool ? "Yes" : "No"
+                    }
+        
+*/
