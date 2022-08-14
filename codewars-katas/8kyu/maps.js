902f6b4feb1459404f18d1625644b6d4d1d74156
Chinwuba Israel <chiizzy5@gmail.com>
2022-08-14 20:39:44 +0100

    8kyu challenge solved

---
+++ codewars-katas/8kyu/maps.js
@@ -0,0 +1,15 @@
+/*  *****
+Challenge: 8 kyu Lost without a map
+
+Instructions: Given an array of integers, return a new array with each value doubled.
+
+For example:
+
+[1, 2, 3] --> [2, 4, 6]
+
+                                       MY Solution
+
+                function maps(x){
+                    return x.map( a => a * 2)
+                }
+*/
