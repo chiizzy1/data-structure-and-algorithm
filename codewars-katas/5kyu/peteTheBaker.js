874e858f9f2b980c3e03d0e0484129ee179749f0
Chinwuba Israel <chiizzy5@gmail.com>
2022-08-20 13:27:07 +0100

    updated pete the baker 5kyu solution

--- codewars-katas/5kyu/peteTheBaker.js
+++ codewars-katas/5kyu/peteTheBaker.js
@@ -11,7 +11,23 @@ cakes({flour: 500, sugar: 200, eggs: 1}, {flour: 1200, sugar: 1200, eggs: 5, mil
 cakes({apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100}, {sugar: 500, flour: 2000, milk: 2000}); 
               
 
-                                       MY Solution
+                                        My New Solution
+
+                 function cakes(recipe, available) {
+                    let maxCake = 1000000
+                    for (const item in recipe) {
+                        if (available[item] === undefined){ return 0 } 
+                        let value = Math.floor( available[item] / recipe[item])
+                        value < maxCake ? maxCake = value : maxCake
+                        
+                    }
+                    
+                    return maxCake
+                }
+
+
+                                       MY Old Solution (:
+
 function cakes(recipe, available) {
  if (Object.keys(recipe).length > Object.keys(available).length){
      return 0
@@ -50,36 +66,4 @@ function cakes(recipe, available) {
 
                 
 */
-
-/**Alternative Solution
- * 
- * A function to determine how many cakes can Pete do, following a recipe with n ingredients available.
- * 
- * @param {Object} recipe an object with the recipe description
- * @param {Object} ingredients an object that tells the function which ingredients does Pete have available.
- * 
- * @returns {Number} the amount of cakes that Pete can do with the given ingredients.
- */
- function cakes(recipe, ingredients) {
-    const recipeKeys = Object.keys(recipe);
-    let cakes = null;
-
-    // Uses a little hacky way to set the cakes variable back to 0 if there is an ingredient lacking.
-    // I use a OR operator, then, if the property doesn't exists inside the ingredients object, it will
-    // try to evaluate the right side of the OR operator. The right side uses a AND operator to break the loop.
-    // At the left side of the AND operator, we have the attribution of cakes to 0 (this is evaluated as truthy)
-    // At the right side of the AND operator, we have a forced false. Then, this condition will never be true,
-    // only executing the left side of the AND, and then, breaking the for loop.
-    for (let i = 0; i < recipeKeys.length && (ingredients[recipeKeys[i]] || (cakes = 0 && false)); i++) {
-        const amount = Math.floor(ingredients[recipeKeys[i]] / recipe[recipeKeys[i]]);
-        
-        if (cakes === null || amount < cakes)
-            cakes = amount;
-    }
-
-    return cakes || 0;
-}
-
-
-
 
