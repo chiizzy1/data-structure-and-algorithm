7c0b24feb904e217c23281de175df14b8e36f72f
Chinwuba Israel <chiizzy5@gmail.com>
2022-08-30 11:10:56 +0100

    8kyu string method kata solved

---
+++ codewars-katas/8kyu/sortAndStar.js
@@ -0,0 +1,19 @@
+/*  *****
+Challenge: 8kyu Sort and Star
+
+Instructions:  You will be given a list of strings. You must sort it alphabetically (case-sensitive, and based on the ASCII values of the chars) and then return the first value.
+
+The returned value must be a string, and have "***" between each of its letters.
+
+You should not remove or add elements from/to the array.
+
+
+                                    MySolution
+                                    
+                 function twoSort(s) {
+                   return s.sort()[0].split('').join('***')
+                }
+
+                   
+                 
+*/
