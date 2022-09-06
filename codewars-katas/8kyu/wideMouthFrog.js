b37189af7eeb7c7e0c7d32cca6fefa21a4bd77c9
Chinwuba Israel <chiizzy5@gmail.com>
2022-09-06 12:08:43 +0100

    8kyu string methos kata solved

---
+++ codewars-katas/8kyu/wideMouthFrog.js
@@ -0,0 +1,12 @@
+/*  *****
+Challenge: 8kyu  The Wide-Mouthed frog!
+
+Instructions: Your goal in this kata is to create complete the mouth_size method this method takes one argument animal which corresponds to the animal encountered by the frog. If this one is an alligator (case-insensitive) return small otherwise return wide
+
+                                MySolution
+
+                function mouthSize(animal) {
+                    return animal.toLowerCase() === "alligator" ? `small` : `wide`
+                }
+                 
+*/
