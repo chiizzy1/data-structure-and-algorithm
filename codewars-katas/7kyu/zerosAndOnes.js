ae55f49a9cf3fff83fff43c5d6884f78b2440d12
Chinwuba Israel <chiizzy5@gmail.com>
2022-09-09 09:17:05 +0100

    7kyu array method kata solved

---
+++ codewars-katas/7kyu/zerosAndOnes.js
@@ -0,0 +1,17 @@
+/*  *****
+Challenge: 7kyu  Ones and Zeros
+
+Instructions:  Testing: [0, 0, 0, 1] ==> 1
+Testing: [0, 0, 1, 0] ==> 2
+Testing: [0, 1, 0, 1] ==> 5
+Testing: [1, 0, 0, 1] ==> 9
+Testing: [0, 0, 1, 0] ==> 2
+Testing: [0, 1, 1, 0] ==> 6
+Testing: [1, 1, 1, 1] ==> 15
+Testing: [1, 0, 1, 1] ==> 11
+
+                                MySolution
+            const binaryArrayToNumber = arr => {
+              return parseInt(arr.join(''), 2)
+            };
+*/
