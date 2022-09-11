344aab37668a1c44c65b1140e1ef54e7d750a1ea
Chinwuba Israel <chiizzy5@gmail.com>
2022-09-11 17:15:51 +0100

    8kyu array method kata solved

---
+++ codewars-katas/8kyu/reduceButGrow.js
@@ -0,0 +1,13 @@
+/*  *****
+Challenge: 8kyu  Beginner - Reduce but Grow
+
+Instructions: [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+
+                                MySolution
+
+                function grow(x){
+  return x.reduce((a,b) => a * b, 1)
+}
+                        
+                            
+*/
