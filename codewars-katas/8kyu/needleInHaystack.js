439a98c32292fb113acce2c2b0e33be63320346f
Chinwuba Israel <chiizzy5@gmail.com>
2022-09-12 10:06:32 +0100

    8kyu array method kata solved

---
+++ codewars-katas/8kyu/needleInHaystack.js
@@ -0,0 +1,14 @@
+/*  *****
+Challenge: 8kyu A Needle in the Haystack
+
+Instructions:  ["hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"] --> "found the needle at position 5" 
+
+
+                                    MySolution
+
+                function findNeedle(haystack) {
+                    let index = haystack.indexOf("needle")
+                    return `found the needle at position ${index}`
+                }
+                
+*/
