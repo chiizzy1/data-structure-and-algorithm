ceb43e7d84295f9908111dd73237ea3369a80d26
Chinwuba Israel <chiizzy5@gmail.com>
2022-09-15 06:39:18 +0100

    leetcode max consecutive  optimized solution added

--- leetcode/easy/maxConsecutiveOnes.js
+++ leetcode/easy/maxConsecutiveOnes.js
@@ -29,4 +29,31 @@ Explanation: The first two digits or the last three digits are consecutive 1s. T
                     return ans
                 };
                 
+                        Optimized Solution
+                
+
+                var findMaxConsecutiveOnes = function(arr) {
+                    let count = 0;
+                
+                    // initialize max
+                    let result = 0;
+                
+                    for (let i = 0; i < arr.length; i++) {
+                        // Reset count when 0 is found
+                        if (arr[i] == 0)
+                            count = 0;
+                
+                        // If 1 is found, increment
+                        // count and update result
+                        // if count becomes more.
+                        else {
+                            // increase count
+                            count++;
+                            result = Math.max(result, count);
+                        }
+                    }
+ 
+                    return result;
+                };
+
 */
