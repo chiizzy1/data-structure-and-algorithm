50f53c4f569a5e04517874f1995383058720021d
Chinwuba Israel <chiizzy5@gmail.com>
2022-10-05 20:12:05 +0100

    8kyu saolved

---
+++ codewars-katas/8kyu/reverseHead.js
@@ -0,0 +1,16 @@
+
+
+/*  *****
+Challenge: 8kyu My head is at the wrong end!
+
+Instructions:  
+
+                                 MySolution
+
+                  function fixTheMeerkat(arr) {
+  return arr.reverse();
+}  
+                  
+                             Codewars Solution
+             const fixTheMeerkat = ([tail, body, head]) => [head, body, tail]       
+*/
