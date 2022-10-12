5ae88308e1ca4358838eb261417557d2694f04b1
Chinwuba Israel <chiizzy5@gmail.com>
2022-10-12 11:13:39 +0100

    8k kata solved

---
+++ codewars-katas/8kyu/csvRepresentation.js
@@ -0,0 +1,15 @@
+
+/*  *****
+Challenge: 8kyu  CVR representation
+
+Instructions:  
+
+                                    MySolution
+            
+              function toCsvText(array) {
+                    return array.map((arr, i) => i === array.length - 1 ? arr.join(',') : `${arr.join(',')}\n` ).join('')
+                }
+                                    CodeWars Solution
+
+            
+*/
