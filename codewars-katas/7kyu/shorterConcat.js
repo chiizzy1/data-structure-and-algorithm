9f718de3073f650b794abfdd8c4684617276868d
chinwuba Israel <chiizzy5@gmail.com>
2022-11-01 12:36:59 +0100

    7kyu challenge solve

---
+++ codewars-katas/7kyu/shorterConcat.js
@@ -0,0 +1,18 @@
+/*  *****
+Challenge:  7kyu  shorter concat [reverse longer]
+
+Instructions:  Given 2 strings, a and b, return a string of the form: shorter+reverse(longer)+shorter.
+
+In other words, the shortest string has to be put as prefix and as suffix of the reverse of the longest.
+
+Strings a and b may be empty, but not null (In C# strings may also be null. Treat them as if they are empty.).
+If a and b have the same length treat a as the longer producing b+reverse(a)+b
+
+
+                                    MySolution
+
+               function shorter_reverse_longer(a,b){
+                    return a.length >= b.length ? b + a.split('').reverse().join("") + b : a + b.split('').reverse().join("") + a
+                }
+               
+*/
