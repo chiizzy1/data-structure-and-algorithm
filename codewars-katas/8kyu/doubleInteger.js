3d31be151ef318936a35445d7bcadf8af972ad4f
Chinwuba Israel <chiizzy5@gmail.com>
2023-04-16 21:32:01 +0100

    oak

---
+++ codewars-katas/8kyu/doubleInteger.js
@@ -0,0 +1,12 @@
+/*  *****
+Challenge: 8kyu double integer
+
+Instructions:  
+
+                                Solution
+        function doubleInteger(i) {
+  // i will be an integer. Double it and return it.
+  return i * 2;
+}
+
+*/
